f1995b9e3d97c2442338358b02b2593de6b15632
ubuntu <najib.mahjoubi@gmail.com>
2018-03-16 09:15:51 +0100

    create all permutation for stacking methods

---
+++ python/get_stacking_approach.py
@@ -0,0 +1,17 @@
+
+from itertools import permutations
+
+
+def get_stacking_approach():
+    classification_models = ["logistic_regression", "decision_tree", "random_forest", "multilayer_perceptron",
+                             "one_vs_rest"]
+    regression_models = ["linear_regression", "decision_tree", "random_forest", "gbt_regressor"]
+
+    available_models = [("classification_model", model) for model in classification_models]
+    available_models += [("regression_model", model) for model in regression_models]
+
+    approaches = []
+    for n_permutation in range(2, len(available_models)+1):
+        approaches += list(permutations(available_models, n_permutation))
+
+    return {"{0}_approach".format(index+1): approach for index, approach in enumerate(approaches)}
