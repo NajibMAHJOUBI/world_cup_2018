fb549e936862ca0bc1f66f0641ac71df610f7493
ubuntu <najib.mahjoubi@gmail.com>
2018-03-16 09:14:09 +0100

    featurization for 2006, 2010, 2014, 2018

--- python/featurization_data.py
+++ python/featurization_data.py
@@ -179,11 +179,11 @@ class FeaturizationData:
 
 if __name__ == "__main__":
     spark = get_spark_session("World_Cup")
-    years = ["2014", "2010", "2006"]
-    confederations = ["AFC", "CAF", "CONCACAF", "CONMEBOL", "OFC", "playoffs", "UEFA", "WCP"]
-    # years = ["2014"]
-    # confederations = ["AFC", "CAF", "CONCACAF", "CONMEBOL", "OFC", "playoffs", "UEFA"]
+    # years = ["2014", "2010", "2006"]
+    # confederations = ["AFC", "CAF", "CONCACAF", "CONMEBOL", "OFC", "playoffs", "UEFA", "WCP"]
+    years = ["2018"]
+    confederations = ["AFC", "CAF", "CONCACAF", "CONMEBOL", "OFC", "playoffs", "UEFA"]
     for year in years:
         print("Year: {0}".format(year))
-        featurization_data = FeaturizationData(spark, year, confederations, "./test/training", "./test/string_indexer")
+        featurization_data = FeaturizationData(spark, year, confederations, "./test/training", "./test/indexer")
         featurization_data.run()
